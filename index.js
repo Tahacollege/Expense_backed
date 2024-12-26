44a9c23c3cd40f682100361a2994102801f884f4
Taha <110016145+Tahacollege@users.noreply.github.com>
2024-12-26 23:35:25 +0530

    cors

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const ConnectRoute=require('./routes/Connect.Route');
 
 const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173','https://expense-backed.onrender.com','https://burhaniexpenses.netlify.app'];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 
 app.use(express.json());
 app.use(bodyParser.json());
