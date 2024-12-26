28b5863aaa82d2a7b3731a8648809f7199e840cc
Taha <110016145+Tahacollege@users.noreply.github.com>
2024-12-26 23:42:06 +0530

    cors

--- index.js
+++ index.js
@@ -11,17 +11,30 @@ const DebitRoute=require('./routes/Debit.Routes')
 const RollRoute=require('./routes/Roll.Routes');
 const ConnectRoute=require('./routes/Connect.Route');
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173','https://expense-backed.onrender.com','https://burhaniexpenses.netlify.app'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'https://expense-backed.onrender.com',
+  'https://burhaniexpenses.netlify.app'
+];
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.error('Blocked by CORS:', origin); // Debug log
       callback(new Error('Not allowed by CORS'));
     }
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true // If cookies or auth tokens are needed
 }));
+
+// Preflight requests
+app.options('*', cors());
+
 
 app.use(express.json());
 app.use(bodyParser.json());
@@ -33,6 +46,7 @@ app.use('/api/roll', RollRoute);
 app.use('/api/connect', ConnectRoute);
 app.get("/", async(req, res) => {
     var data=await conn
+    res.send('connected ')
 });
 
 
